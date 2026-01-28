ad3a5426d6b4f2e7ec4825ab40629209cb0d3e74
JayRGarg <jayrgarg@gmail.com>
2026-01-27 23:12:25 -0600

    implement task 3

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -19,8 +19,13 @@ template <class T>
 class MessageQueue
 {
 public:
+    T receive();
+    void send(T &&msg);
 
 private:
+    std::deque<T> _queue;
+    std::condition_variable _cv;
+    std::mutex _mtx;
     
 };
 
